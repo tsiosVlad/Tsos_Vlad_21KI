6fe6d1ada73d2f7fda624e1696bfc0a662d1a3b5
Влад Цьось <cosvlad426@gmail.com>
2026-09-08 22:27:54 +0300

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,42 @@
+print("Hello, World!")
+
+a = 16
+b = -8
+c = 2.25
+d = -0.002
+name = "Влад"
+surname = "Цьось"
+is_student = True
+is_teacher = False
+comp = 5 + 7j
+my_list = [10, 20, 30, "four", 50.5]
+my_tuple = (100, 200, 300)
+my_set = {4, 5, 6, 6, 5}
+my_dict = {"name": "Влад", "age": 16}
+empty_value = None
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(name, type(name))
+print(surname, type(surname))
+print(is_student, type(is_student))
+print(is_teacher, type(is_teacher))
+print(comp, type(comp))
+print(my_list, type(my_list))
+print(my_tuple, type(my_tuple))
+print(my_set, type(my_set))
+print(my_dict, type(my_dict))
+print(empty_value, type(empty_value))
+
+x = 17
+y = 6
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
